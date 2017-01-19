e75833c581dd5f81435cd13885df34f32987316f
bwoodbury3 <brianj.woodbury@gmail.com>
2017-01-19 18:54:52 -0500

    Sample login code with firebase auth

--- app/src/main/java/com/example/noah/onthefly/activities/ActivityLogin.java
+++ app/src/main/java/com/example/noah/onthefly/activities/ActivityLogin.java
@@ -3,15 +3,25 @@ package com.example.noah.onthefly.activities;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.noah.onthefly.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ActivityLogin extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     EditText usernameField;
     EditText passwordField;
     Button login;
@@ -23,6 +33,35 @@ public class ActivityLogin extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         inputSetup();
         buttonSetup();
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                /*
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                */
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     protected void inputSetup() {
@@ -35,7 +74,22 @@ public class ActivityLogin extends AppCompatActivity {
         forgotPass = (Button) findViewById(R.id.forgot_password_button);
     }
 
-    protected void login(View loginButton) {
+    protected void onLoginClick(View v) {
+        mAuth.signInWithEmailAndPassword(usernameField.getText().toString(),
+                passwordField.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            login();
+                        } else {
+                            System.out.println("Auth not successful");
+                        }
+                    }
+                });
+    }
+
+    protected void login() {
         Intent flightListIntent = new Intent(this, ActivityFlightList.class);
         this.startActivity(flightListIntent);
     }
