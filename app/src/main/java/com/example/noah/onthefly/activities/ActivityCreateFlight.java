7af5d1f727d3a7676a4a6bf29999a45e3cc42560
Noah Harasz <Noah Harasz>
2017-01-22 14:48:09 -0500

    changed password field hint typeface on login screen

--- app/src/main/java/com/example/noah/onthefly/activities/ActivityCreateFlight.java
+++ app/src/main/java/com/example/noah/onthefly/activities/ActivityCreateFlight.java
@@ -2,14 +2,24 @@ package com.example.noah.onthefly.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.noah.onthefly.R;
 
 public class ActivityCreateFlight extends AppCompatActivity {
+    EditText firstNameField;
+    EditText lastNameField;
+    EditText emailField;
+    EditText passwordField;
+    EditText confirmPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acivity_create_flight);
+        inputSetup();
+    }
+
+    protected void inputSetup() {
     }
 }
--- app/src/main/java/com/example/noah/onthefly/activities/ActivityLogin.java
+++ app/src/main/java/com/example/noah/onthefly/activities/ActivityLogin.java
@@ -3,8 +3,10 @@ package com.example.noah.onthefly.activities;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -30,6 +32,8 @@ public class ActivityLogin extends AppCompatActivity {
     protected void inputSetup() {
         usernameField = (EditText) findViewById(R.id.username_input);
         passwordField = (EditText) findViewById(R.id.password_input);
+        passwordField.setTypeface(Typeface.DEFAULT);
+        passwordField.setTransformationMethod(new PasswordTransformationMethod());
     }
 
     protected void buttonSetup() {
