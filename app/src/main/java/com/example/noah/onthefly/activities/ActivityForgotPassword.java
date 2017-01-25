bc4ec2d1a4b762ade1bd9806393bb1bb2b764129
Brian Woodbury <bwoodbury3@gatech.edu>
2017-01-24 19:27:48 -0800

    Confirmation dialogue routes to the login page

--- app/src/main/java/com/example/noah/onthefly/activities/ActivityForgotPassword.java
+++ app/src/main/java/com/example/noah/onthefly/activities/ActivityForgotPassword.java
@@ -1,12 +1,16 @@
 package com.example.noah.onthefly.activities;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import com.example.noah.onthefly.R;
-import com.example.noah.onthefly.fragments.FragmentPasswordReset;
 
 public class ActivityForgotPassword extends AppCompatActivity {
 
@@ -22,7 +26,21 @@ public class ActivityForgotPassword extends AppCompatActivity {
 
     protected void forgotPassword(View v) {
         String email = emailField.getText().toString();
-        FragmentPasswordReset fragmentPasswordReset = new FragmentPasswordReset(this, email);
-        fragmentPasswordReset.show();
+        AlertDialog.Builder passwordResetConfirmation = new AlertDialog.Builder(this,
+                R.style.PasswordResetConfirmationDialog);
+        passwordResetConfirmation.setTitle("Password has been reset");
+        passwordResetConfirmation
+                .setMessage("Please check your email to reset your password.")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Context c = ((Dialog) dialog).getContext();
+                        Intent loginIntent = new Intent(c, ActivityLogin.class);
+                        c.startActivity(loginIntent);
+                    }
+                });
+        AlertDialog confirmationDialog = passwordResetConfirmation.create();
+        confirmationDialog.show();
     }
 }
