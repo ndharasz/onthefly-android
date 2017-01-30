cec693cc0862cad82030d04741d4b0027cf355f7
Brian Woodbury <bwoodbury3@gatech.edu>
2017-01-30 14:40:12 -0800

    login firebase example

--- app/src/main/java/com/example/noah/onthefly/activities/ActivityLogin.java
+++ app/src/main/java/com/example/noah/onthefly/activities/ActivityLogin.java
@@ -4,15 +4,22 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.example.noah.onthefly.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ActivityLogin extends AppCompatActivity {
     EditText usernameField;
@@ -21,12 +28,43 @@ public class ActivityLogin extends AppCompatActivity {
     Button forgotPass;
     CheckBox rememberMe;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String TAG = "ActivityLogin";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         inputSetup();
         buttonSetup();
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is logged in
+                } else {
+                    // Display error message?
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     protected void inputSetup() {
@@ -43,8 +81,24 @@ public class ActivityLogin extends AppCompatActivity {
     }
 
     protected void login(View loginButton) {
-        Intent flightListIntent = new Intent(this, ActivityFlightList.class);
-        this.startActivity(flightListIntent);
+        Log.d(TAG, "Task started");
+        mAuth.createUserWithEmailAndPassword(usernameField.getText().toString(),
+                    passwordField.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "Login task complete");
+                if (task.isSuccessful()) {
+                    Intent flightListIntent = new Intent(ActivityLogin.this,
+                                ActivityFlightList.class);
+                    ActivityLogin.this.startActivity(flightListIntent);
+                } else {
+                    Log.d(TAG, "Login failed.");
+                    // print error message?
+                }
+            }
+        });
+
     }
 
     protected void forgotPassword(View v) {
