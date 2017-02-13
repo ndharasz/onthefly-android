ea281f5581e7f4a8bc1641ba62e34ff603bab291
bwoodbury3 <brianj.woodbury@gmail.com>
2017-02-13 12:46:25 -0500

    flights load from the database and display in the list

--- app/src/main/java/com/example/noah/onthefly/activities/ActivityFlightList.java
+++ app/src/main/java/com/example/noah/onthefly/activities/ActivityFlightList.java
@@ -9,19 +9,81 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.noah.onthefly.R;
+import com.example.noah.onthefly.models.Flight;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.zip.Inflater;
+
+import static com.example.noah.onthefly.R.layout.item_flight;
 
 public class ActivityFlightList extends AppCompatActivity {
     private SharedPreferences loginPrefs;
     private SharedPreferences.Editor loginPrefsEditor;
+    private TableLayout flightTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_list);
+        flightTable = (TableLayout) findViewById(R.id.flight_table);
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("flights");
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Flight flight = (Flight) dataSnapshot.getValue(Flight.class);
+                // ignore this data if it doesn't belong to this used
+                if (!flight.getUserid().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                    return;
+                }
+
+                TableRow v = (TableRow) LayoutInflater.from(ActivityFlightList.this).inflate(item_flight, null);
+
+                ((Button)v.findViewById(R.id.upcoming_flight_edit)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(ActivityFlightList.this, ActivityEditFlight.class));
+                    }
+                });
+
+                ((TextView) v.findViewById(R.id.upcoming_flight_date)).setText(flight.getDate());
+                ((TextView) v.findViewById(R.id.upcoming_flight_arrival)).setText(flight.getArriveAirport());
+                ((TextView) v.findViewById(R.id.upcoming_flight_depart)).setText(flight.getDepartAirport());
+                flightTable.addView(v);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 //      for future dynamic adding usage:
 //        LayoutInflater item = (LayoutInflater)
 //                this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
